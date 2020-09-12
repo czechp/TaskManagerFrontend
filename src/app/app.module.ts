6c4cba8a494eb43fa022ec47026a8eeaca957573
P Czech <webcoderc@gmail.com>
2020-09-12 07:59:43 +0200

    Created component to login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { LeftNavbarComponent } from './components/left-navbar/left-navbar.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    LeftNavbarComponent
+    LeftNavbarComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
