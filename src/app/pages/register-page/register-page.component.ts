1245e79678c26d26a99f59cd65edb5792ffa224a
PCzech <webcoderc@gmail.com>
2020-09-20 21:56:57 +0200

    Validate email addres

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HttpApiService } from '../../services/httpApiService/http-api.service';
 import { MIN_PASSWORD_LENGTH, MIN_USERNAME_LENGTH } from '../../consts/Constants';
 import { ViewCustomizerService } from '../../services/viewCustomizer/view-customizer.service';
-import { usernameExistenceEndpoint, usersEndpoint } from '../../services/URL';
+import { emailExistenceEndpoint, usernameExistenceEndpoint, usersEndpoint } from '../../services/URL';
 
 @Component({
   selector: 'app-register-page',
@@ -10,10 +10,26 @@ import { usernameExistenceEndpoint, usersEndpoint } from '../../services/URL';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
+  private usernameValidated = false;
+  private passwordValidated = false;
+  private passowrdConfValidated = false;
+  private emailValidated = false;
+
   public statement: string;
+
+
 
   @ViewChild('username')
   public loginInput: ElementRef;
+
+  @ViewChild('password')
+  public passwordInput: ElementRef;
+
+  @ViewChild('passwordConf')
+  public passwordConfInput: ElementRef;
+
+  @ViewChild('email')
+  public emailInput: ElementRef;
 
   constructor(
     private httpApiService: HttpApiService,
@@ -25,7 +41,7 @@ export class RegisterPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public validateUsername(username: string) {
+  public validateUsername(username: string): void {
     this.clearStatement();
     this.viewCustomizerService.resetColors(this.loginInput);
     if (username.length >= MIN_USERNAME_LENGTH) {
@@ -36,19 +52,63 @@ export class RegisterPageComponent implements OnInit {
             this.viewCustomizerService.setDangerColors(this.loginInput);
           },
           error => {
+            this.usernameValidated = true;
           }
         );
     } else {
       this.statement = 'Błąd! Login za krótki. Minimum ' + MIN_USERNAME_LENGTH + ' znaków';
       this.viewCustomizerService.setDangerColors(this.loginInput);
+      this.usernameValidated = false;
     }
   }
 
   public validatePassowrd(password: string): void {
+    this.clearStatement();
+    this.viewCustomizerService.resetColors(this.passwordInput)
     if (password.length < MIN_PASSWORD_LENGTH) {
-      this.statement = 'Błąd! Hasło za krótkie. Minumum ' + MIN_PASSWORD_LENGTH + 'znaków';
+      this.statement = 'Błąd! Hasło za krótkie. Minumum ' + MIN_PASSWORD_LENGTH + ' znaków';
+      this.viewCustomizerService.setDangerColors(this.passwordInput);
+      this.passwordValidated = false;
+    } else {
+      this.passwordValidated = true;
     }
   }
+
+  public validatePasswordEquals(password: string, passwordConf: string): void {
+    this.clearStatement();
+    this.viewCustomizerService.resetColors(this.passwordConfInput);
+    if (password !== passwordConf) {
+      this.statement = "Błąd! Hasła są różne"
+      this.viewCustomizerService.setDangerColors(this.passwordConfInput);
+      this.passowrdConfValidated = false;
+    } else {
+      this.passowrdConfValidated = true;
+    }
+  }
+
+  public validateEmail(email: string) {
+    this.viewCustomizerService.resetColors(this.emailInput);
+    let regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (regExp.test(email)) {
+      this.httpApiService.head(usersEndpoint + emailExistenceEndpoint, [{ name: 'email', parameter: email }])
+        .subscribe(
+          (next: any) => {
+            this.viewCustomizerService.setDangerColors(this.emailInput);
+            this.statement = 'Błąd! Taki email jest już wykorzystany.'
+            this.emailValidated = false;
+          },
+          (error: any) => {
+            this.emailValidated = true;
+          }
+        )
+    } else {
+      this.emailValidated = false;
+      this.viewCustomizerService.setDangerColors(this.emailInput);
+      this.statement = 'Błąd! Nieporawny format adresu email';
+    }
+  }
+
+
 
   private clearStatement(): void {
     this.statement = '';
--- src/app/services/URL.ts
+++ src/app/services/URL.ts
@@ -2,3 +2,6 @@ export const URL = 'http://localhost:8080/api';
 export const loginEndpoint = '/users/login';
 export const usersEndpoint = '/users';
 export const usernameExistenceEndpoint = '/username';
+export const emailExistenceEndpoint = '/email';
+
+
