5a46eb9c27156a13a6b5bbfb16450c410a8c0de1
PCzech <webcoderc@gmail.com>
2020-09-20 21:07:06 +0200

    Validate password #1

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,8 +1,8 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {HttpApiService} from '../../services/httpApiService/http-api.service';
-import {MIN_USERNAME_LENGTH} from '../../consts/Constants';
-import {ViewCustomizerService} from '../../services/viewCustomizer/view-customizer.service';
-import {usernameExistenceEndpoint, usersEndpoint} from '../../services/URL';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HttpApiService } from '../../services/httpApiService/http-api.service';
+import { MIN_PASSWORD_LENGTH, MIN_USERNAME_LENGTH } from '../../consts/Constants';
+import { ViewCustomizerService } from '../../services/viewCustomizer/view-customizer.service';
+import { usernameExistenceEndpoint, usersEndpoint } from '../../services/URL';
 
 @Component({
   selector: 'app-register-page',
@@ -29,7 +29,7 @@ export class RegisterPageComponent implements OnInit {
     this.clearStatement();
     this.viewCustomizerService.resetColors(this.loginInput);
     if (username.length >= MIN_USERNAME_LENGTH) {
-      this.httpApiService.head(usersEndpoint + usernameExistenceEndpoint, [{name: 'username', parameter: username}])
+      this.httpApiService.head(usersEndpoint + usernameExistenceEndpoint, [{ name: 'username', parameter: username }])
         .subscribe(
           response => {
             this.statement = 'Błąd! Taka nazwa użytkownika już istnieje';
@@ -39,8 +39,14 @@ export class RegisterPageComponent implements OnInit {
           }
         );
     } else {
-      this.statement = 'Błąd! Login za krótki';
+      this.statement = 'Błąd! Login za krótki. Minimum ' + MIN_USERNAME_LENGTH + ' znaków';
       this.viewCustomizerService.setDangerColors(this.loginInput);
+    }
+  }
+
+  public validatePassowrd(password: string): void {
+    if (password.length < MIN_PASSWORD_LENGTH) {
+      this.statement = 'Błąd! Hasło za krótkie. Minumum ' + MIN_PASSWORD_LENGTH + 'znaków';
     }
   }
 
