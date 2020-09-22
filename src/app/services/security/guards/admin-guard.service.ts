cc8dc7d0d7bf4fd3b6e1388818ddee3af68e115b
PCzech <you@example.com>
2020-09-22 23:17:00 +0200

    Display info about user

--- src/app/services/security/guards/admin-guard.service.ts
+++ src/app/services/security/guards/admin-guard.service.ts
@@ -14,7 +14,8 @@ export class AdminGuardService implements CanActivate {
   ) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+   Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.authorizationService.isLogged()
       && this.authorizationService.getRole() === 'ADMIN') {
       return true;
